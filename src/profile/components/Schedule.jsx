9e2e827e059d63aedd7991a7ab15881048dfa326
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-10-28 10:01:41 -0500

    Select chedule.

--- src/profile/components/Schedule.jsx
+++ src/profile/components/Schedule.jsx
@@ -1,6 +1,20 @@
-import { FormControl, FormLabel, VStack } from '@chakra-ui/react'
-import { Select, Tag, TimePicker } from 'antd'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from '@chakra-ui/react'
+import { Select, Tag } from 'antd'
 import React, { useState } from 'react'
+import { Select as ChakraSelect } from '@chakra-ui/react'
+import { IoMdAddCircleOutline } from 'react-icons/io'
+import { v4 as uuidv4 } from 'uuid'
+import { AiOutlineDelete } from 'react-icons/ai'
 
 const options = [
   {
@@ -46,25 +60,71 @@ const tagRender = (props) => {
   )
 }
 
-const Time = () => {
-  const [value, setValue] = useState(null)
-  const onChange = (time) => {
-    setValue(time)
-  }
+const Duration = ({ getFieldProps = (f) => f, name = 'duration' }) => {
+  const options = [
+    {
+      name: '10min',
+      id: 1,
+    },
+    {
+      name: '20min',
+      id: 2,
+    },
+    {
+      name: '30min',
+      id: 3,
+    },
+    {
+      name: '40min',
+      id: 4,
+    },
+    {
+      name: '50min',
+      id: 5,
+    },
+  ]
   return (
-    <TimePicker
-      value={value}
-      onChange={onChange}
-      style={{
-        width: '100%',
-      }}
-      placeholder="Seleccionar hora"
-      size="large"
-    />
+    <ChakraSelect
+      placeholder="Selecciona una opción"
+      name={name}
+      {...getFieldProps(name)}
+    >
+      {options.map(({ name, id }) => (
+        <option key={name} value={name}>
+          {name}
+        </option>
+      ))}
+    </ChakraSelect>
   )
 }
 
 export const Schedule = () => {
+  const [hours, setHours] = useState([{ id: uuidv4(), name: '00:00' }])
+  const handleDelete = (hourId) => {
+    const newHours = hours.filter(({ name, id }) => id !== hourId)
+    setHours(newHours)
+  }
+  const onChange = (e) => {
+    const newHours = hours.map(({ name, id }) => {
+      if (e.target.id === id) {
+        return {
+          id,
+          name: e.target.value,
+        }
+      } else {
+        return {
+          id,
+          name,
+        }
+      }
+    })
+    setHours(newHours)
+  }
+  const handleAddHour = () => {
+    setHours([...hours, { id: uuidv4(), name: '00:00' }])
+  }
+
+  console.log({ hours })
   return (
     <VStack width="full" spacing="6">
       <FormControl id="days">
@@ -78,12 +138,43 @@ export const Schedule = () => {
           }}
           options={options}
           size="large"
+          placeholder="Seleccione los días"
         />
       </FormControl>
-
+      <FormControl id="duration">
+        <FormLabel>Duración</FormLabel>
+        <Duration />
+      </FormControl>
       <FormControl id="time">
-        <FormLabel>Hora</FormLabel>
-        <Time />
+        <FormLabel display={'flex'}>
+          Hora
+          <div
+            style={{
+              flexGrow: 1,
+            }}
+          />
+          <IconButton size="xs" colorScheme={'blue'} onClick={handleAddHour}>
+            <IoMdAddCircleOutline fontSize={'15px'} />
+          </IconButton>
+        </FormLabel>
+        <Box display={'flex'} flexDirection="column" gap="10px">
+          {hours.map(({ name: hour, id }) => (
+            <InputGroup>
+              <Input
+                key={id}
+                id={id}
+                type="time"
+                onChange={onChange}
+                value={hour}
+              />
+              <InputRightElement>
+                <IconButton size="xs" onClick={() => handleDelete(id)}>
+                  <AiOutlineDelete />
+                </IconButton>
+              </InputRightElement>
+            </InputGroup>
+          ))}
+        </Box>
       </FormControl>
     </VStack>
   )
